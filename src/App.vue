3201d027c1676c603f46816ed1c2176774e5d548
potatoMM <m_littlepotato@163.com>
2020-08-27 10:17:27 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hi</h1>
+  <Test/>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>this is first components</div>
+</template>
