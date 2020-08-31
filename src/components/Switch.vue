cd574a6282c50922787e134ecaed978fb7d405ce
potatoMM <m_littlepotato@163.com>
2020-08-31 10:36:16 +0800

    switch组件完成

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <Switch/>
+    <div>
+      基本用法：<br />
+      <Switch v-model:value="value" />
+    </div>
+    <div>
+      自定义颜色：<br />
+      <Switch v-model:value="colorValue" inactiveColor="#ff4949" />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
 export default {
-  components:{Switch}
-}
+  components: { Switch },
+  setup() {
+    const value = ref(false);
+    const colorValue = ref(true);
+    return {
+      value,
+      colorValue,
+    };
+  },
+};
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,50 +1,62 @@
 <template>
-  <button :class="{checked}" @click="togglecheckedStatus"><span></span></button>
+  <button :class="{ checked: value }" @click="togglecheckedStatus" :style="{'backgroundColor':inactiveColor}">
+    <span></span>
+  </button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-  setup(){
-    const checked = ref(false)
+  props: {
+    value: {
+      type:Boolean,
+      required: true,
+    },
+    inactiveColor:{
+      type:String,
+      required: false,
+      default:'#dcdfe6'
+    },
+  },
+  setup(props, context) {
+    const checked = ref(false);
     const togglecheckedStatus = () => {
-      checked.value = !checked.value
-    }
+      context.emit("update:value", !props.value);
+    };
     return {
       checked,
-      togglecheckedStatus
-    }
-  }
-}
+      togglecheckedStatus,
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
-   $h: 20px;
-   $h2: $h - 4px;
-   button{
-     height: $h;
-     width: $h*2;
-     border: none;
-     background-color: #dcdfe6;
-     border-radius: $h/2;
-     position: relative;
-   }
-   span{
-     position: absolute;
-     top: 2px;
-     left: 2px;
-     height: $h2;
-     width: $h2;
-     background:white;
-     border-radius: $h2 / 2;
-     transition: left .25s;
-   }
-   button.checked{
-     background-color: #409eff;
-   }
-   button.checked > span{
-     outline: none;
-    left: calc(100% - #{$h2} - 2px);
-   }
-   button:focus{
-     outline: none;
-   }
- </style> 
+$h: 20px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background-color: #dcdfe6;
+  border-radius: $h/2;
+  position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 0.25s;
+  }
+  &.checked {
+    background-color: #409eff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus{
+    outline: none;
+  }
+}
+</style>
