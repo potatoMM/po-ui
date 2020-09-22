2ec7333ccf28c544c59086dae326f068ab4e6b53
potatoMM <m_littlepotato@163.com>
2020-09-22 15:05:55 +0800

    优化markdown使用

--- src/router.ts
+++ src/router.ts
@@ -1,45 +1,50 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import Switch from './components/Switch.vue'
-import Button from './components/Button.vue'
-import Dialog from './components/Dialog.vue'
-import Tabs from './components//tabs.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import { createWebHashHistory, createRouter } from "vue-router";
+import { h } from "vue";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import Switch from "./components/Switch.vue";
+import Button from "./components/Button.vue";
+import Dialog from "./components/Dialog.vue";
+import Tabs from "./components//tabs.vue";
 
-const history = createWebHashHistory()
+import Markdown from "./components/Markdown.vue";
+
+const history = createWebHashHistory();
+const md = (filename) =>
+  h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+
 const router = createRouter({
   history,
-  routes: [{
-    path: '/',
-    component: Home
-  }, {
-    path: '/doc',
-    component: Doc,
-    children: [{
-      path:'intro',
-      component:Intro
-    },{
-      path:'get-started',
-      component:GetStarted
-    },{
-      path:'install',
-      component:Install
-    },{
-      path:'switch',
-      component:Switch
-    },{
-      path:'button',
-      component:Button
-    },{
-      path:'dialog',
-      component:Dialog
-    },{
-      path:'tabs',
-      component:Tabs
-    }]
-  }]
-})
-export default router
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/doc",
+      component: Doc,
+      children: [
+        { path: "intro", component: md("intro") },
+        { path: "get-started", component: md("get-started") },
+        { path: "install", component: md("install") },
+        {
+          path: "switch",
+          component: Switch,
+        },
+        {
+          path: "button",
+          component: Button,
+        },
+        {
+          path: "dialog",
+          component: Dialog,
+        },
+        {
+          path: "tabs",
+          component: Tabs,
+        },
+      ],
+    },
+  ],
+});
+export default router;
